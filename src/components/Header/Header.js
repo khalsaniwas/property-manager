b508857c81e42afd347cf4925cc2aaa1816b6253
khalsaniwas <honeysaini21@gmail.com>
2019-12-05 21:35:08 -0500

    Added background image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import logo from '../../images/homeIcon3.png'
 
 const authenticatedOptions = (
   <Fragment>
@@ -26,13 +27,14 @@ const Header = ({ user }) => (
   <Navbar bg="dark" variant="dark" expand="md">
     <Navbar.Brand href="#home">
       <img
-        src="https://raw.githubusercontent.com/khalsaniwas/property-manager/master/src/images/homeIcon2.jpeg"
+        src={logo}
         width="50"
         height="50"
         className="d-inline-block align-top"
         alt="Property Manager"
       />
     </Navbar.Brand>
+
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
