6ac0ea52283742a6bb5c50f0f302428f9c15baff
khalsaniwas <honeysaini21@gmail.com>
2019-12-05 22:31:11 -0500

    Fixed property PropertyForm
    Aligned property form fields
    fixed cancel path to get back to properties on property create form

--- src/components/Properties/PropertyForm.js
+++ src/components/Properties/PropertyForm.js
@@ -1,56 +1,79 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 
 const PropertyForm = ({ property, handleSubmit, handleChange }) => (
-  <form onSubmit={handleSubmit}>
-    <label>Name</label>
-    <input
-      placeholder="Property name"
-      value={property.name}
-      name="name"
-      onChange={handleChange}
-      required
-    />
-    <label>Address</label>
-    <input
-      placeholder="enter address"
-      value={property.address}
-      name="address"
-      onChange={handleChange}
-    />
-    <label>Tenant Name</label>
-    <input
-      placeholder="enter name"
-      value={property.tenant_name}
-      name="tenant_name"
-      onChange={handleChange}
-    />
-    <label>Lease start date</label>
-    <input
-      placeholder="MM-DD-YYYY"
-      value={property.lease_start_date}
-      name="lease_start_date"
-      onChange={handleChange}
-    />
-    <label>Lease end date</label>
-    <input
-      placeholder="MM-DD-YYYY"
-      value={property.lease_end_date}
-      name="lease_end_date"
-      onChange={handleChange}
-    />
-    <label>Rent</label>
-    <input
-      placeholder="total rent"
-      value={property.rent}
-      name="rent"
-      onChange={handleChange}
-    />
+  <Form onSubmit={handleSubmit}>
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          placeholder="Property name"
+          value={property.name}
+          name="name"
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridAddress">
+        <Form.Label>Address</Form.Label>
+        <Form.Control
+          placeholder="enter address"
+          value={property.address}
+          name="address"
+          onChange={handleChange}
+        />
+      </Form.Group>
+    </Form.Row>
+
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridTenantName">
+        <Form.Label>Tenant Name</Form.Label>
+        <Form.Control
+          placeholder="enter name"
+          value={property.tenant_name}
+          name="tenant_name"
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridLeaseStartDate">
+        <Form.Label>Lease start date</Form.Label>
+        <Form.Control
+          placeholder="MM-DD-YYYY"
+          value={property.lease_start_date}
+          name="lease_start_date"
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridLeaseEndDate">
+        <Form.Label>Lease end date</Form.Label>
+        <Form.Control
+          placeholder="MM-DD-YYYY"
+          value={property.lease_end_date}
+          name="lease_end_date"
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridRent">
+        <Form.Label>Rent</Form.Label>
+        <Form.Control
+          placeholder="total rent"
+          value={property.rent}
+          name="rent"
+          onChange={handleChange}
+        />
+      </Form.Group>
+    </Form.Row>
     <button type="submit">Submit</button>
-    <Link to="/">
+    <Link to="/properties">
       <button type="text">Cancel</button>
     </Link>
-  </form>
+  </Form>
 )
 
 export default PropertyForm
