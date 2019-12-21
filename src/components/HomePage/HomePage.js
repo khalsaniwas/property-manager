1a93af77bcd13e37e641b6fc251d2abfefd77cd5
khalsaniwas <honeysaini21@gmail.com>
2019-12-20 22:58:17 -0500

    Created new component homepage
    Added welcome text to homepage

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HomePage.scss'
+
+const HomePage = (props) => {
+  return (
+    <div className="text-center">
+      <h3>Welcome to the Property Manager app...</h3>
+      <p className="font-italic font-weight-bold">
+        Our app is for landlords in the smartphone world. Spend less time on paperwork, communicate more effectively and never forget what is important for your rental business. Property manager helps you be a better organized landlord.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
